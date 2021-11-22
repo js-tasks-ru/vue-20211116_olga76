68e2b57fec5ba66bae7db94b5914bcd656329128
o.d.kobozeva <o.d.kobozeva@gmail.com>
2021-11-22 12:17:30 +0300

    Add calculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,28 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
-// Создайте Vue приложение
+const app = createApp({
+  data() {
+    return {
+      numA: null,
+      numB: null,
+      checkedOperator: [],
+    }
+  },
+  computed: {
+    calcResult() {
+      if (this.checkedOperator == 'sum') {
+        return parseFloat(this.numA) + parseFloat(this.numB)
+      } else if (this.checkedOperator == 'subtract') {
+        return parseFloat(this.numA) - parseFloat(this.numB)
+
+      } else if (this.checkedOperator == 'multiply') {
+        return parseFloat(this.numA) * parseFloat(this.numB)
+
+      } else if (this.checkedOperator == 'divide') {
+        return parseFloat(this.numA) / parseFloat(this.numB)
+      }
+    }
+  }
+});
+
+app.mount("#app");
