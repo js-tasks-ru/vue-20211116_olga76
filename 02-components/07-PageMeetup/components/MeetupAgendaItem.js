f4570756cc6d046b23c0c3b725960cc0e57ee44b
o.d.kobozeva <o.d.kobozeva@gmail.com>
2021-11-29 11:50:15 +0300

    Change data to computed agendaItemIcons/agendaItemDefaultTitles

--- 02-components/07-PageMeetup/components/MeetupAgendaItem.js
+++ 02-components/07-PageMeetup/components/MeetupAgendaItem.js
@@ -10,22 +10,25 @@ export default defineComponent({
     },
   },
 
-  data() {
-    return {
-      agendaItemIcons,
-      agendaItemDefaultTitles,
-    };
+  computed: {
+    agendaItemIcons() {
+      return `/assets/icons/icon-${agendaItemIcons[this.agendaItem.type]}.svg`;
+    },
+
+    agendaItemDefaultTitles() {
+      return agendaItemDefaultTitles[this.agendaItem.type];
+    },
   },
 
   template: `
     <div class="agenda-item">
       <div class="agenda-item__col">
-        <img :src="\`/assets/icons/icon-\${agendaItemIcons[agendaItem.type]}.svg\`" class="icon" alt="key" />
+        <img :src="agendaItemIcons" class="icon" alt="key" />
       </div>
       <div class="agenda-item__col">{{ agendaItem.startsAt}} - {{ agendaItem.endsAt }}</div>
       <div class="agenda-item__col">
         <h3 class="agenda-item__title" v-if="agendaItem.title">{{ agendaItem.title }}</h3>
-        <h3 class="agenda-item__title" v-else>{{ agendaItemDefaultTitles[agendaItem.type] }}</h3>
+        <h3 class="agenda-item__title" v-else>{{ agendaItemDefaultTitles }}</h3>
         <p class="agenda-item__talk" v-if="agendaItem.type === 'talk'">
           <span>{{ agendaItem.speaker }}</span>
           <span class="agenda-item__dot"></span>
