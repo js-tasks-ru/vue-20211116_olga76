3fc4998f9269b0067155661219a1b636e125df49
o.d.kobozeva <o.d.kobozeva@gmail.com>
2021-12-19 15:13:00 +0300

    07-forms | Add 03-MeetupAgendaItemForm

--- 07-forms/03-MeetupAgendaItemForm/components/MeetupAgendaItemForm.vue
+++ 07-forms/03-MeetupAgendaItemForm/components/MeetupAgendaItemForm.vue
@@ -1,42 +1,65 @@
 <template>
   <fieldset class="agenda-item-form">
-    <button type="button" class="agenda-item-form__remove-button">
+    <button type="button" class="agenda-item-form__remove-button" @click="$emit('remove')">
       <ui-icon icon="trash" />
     </button>
 
     <ui-form-group>
-      <ui-dropdown title="Тип" :options="$options.agendaItemTypeOptions" name="type" />
+      <ui-dropdown v-model="localAgendaItem.type" title="Тип" :options="$options.agendaItemTypeOptions" name="type" />
     </ui-form-group>
 
     <div class="agenda-item-form__row">
       <div class="agenda-item-form__col">
         <ui-form-group label="Начало">
-          <ui-input type="time" placeholder="00:00" name="startsAt" />
+          <ui-input v-model="localAgendaItem.startsAt" type="time" placeholder="00:00" name="startsAt" />
         </ui-form-group>
       </div>
       <div class="agenda-item-form__col">
         <ui-form-group label="Окончание">
-          <ui-input type="time" placeholder="00:00" name="endsAt" />
+          <ui-input v-model="localAgendaItem.endsAt" type="time" placeholder="00:00" name="endsAt" />
         </ui-form-group>
       </div>
     </div>
 
-    <ui-form-group label="Тема">
-      <ui-input name="title" />
-    </ui-form-group>
-    <ui-form-group label="Докладчик">
-      <ui-input name="speaker" />
-    </ui-form-group>
-    <ui-form-group label="Описание">
-      <ui-input multiline name="description" />
-    </ui-form-group>
-    <ui-form-group label="Язык">
-      <ui-dropdown title="Язык" :options="$options.talkLanguageOptions" name="language" />
-    </ui-form-group>
+    <template v-if="localAgendaItem.type === 'talk'">
+      <ui-form-group label="Тема">
+        <ui-input v-model="localAgendaItem.title" name="title" />
+      </ui-form-group>
+      <ui-form-group label="Докладчик">
+        <ui-input v-model="localAgendaItem.speaker" name="speaker" />
+      </ui-form-group>
+      <ui-form-group label="Описание">
+        <ui-input v-model="localAgendaItem.description" multiline name="description" />
+      </ui-form-group>
+      <ui-form-group label="Язык">
+        <ui-dropdown
+          v-model="localAgendaItem.language"
+          title="Язык"
+          :options="$options.talkLanguageOptions"
+          name="language"
+        />
+      </ui-form-group>
+    </template>
+
+    <template v-else-if="localAgendaItem.type === 'other'">
+      <ui-form-group label="Заголовок">
+        <ui-input v-model="localAgendaItem.title" name="title" />
+      </ui-form-group>
+      <ui-form-group label="Описание">
+        <ui-input v-model="localAgendaItem.description" multiline name="description" />
+      </ui-form-group>
+    </template>
+
+    <template v-else>
+      <ui-form-group label="Нестандартный текст (необязательно)">
+        <ui-input v-model="localAgendaItem.title" name="title" />
+      </ui-form-group>
+    </template>
   </fieldset>
 </template>
 
 <script>
+import { isEqual } from 'lodash-es';
 import UiIcon from './UiIcon';
 import UiFormGroup from './UiFormGroup';
 import UiInput from './UiInput';
@@ -88,6 +111,48 @@ export default {
     agendaItem: {
       type: Object,
       required: true,
+    },
+  },
+
+  emits: ['update:agendaItem', 'remove'],
+
+  data() {
+    return {
+      localAgendaItem: { ...this.agendaItem },
+    };
+  },
+
+  computed: {
+    startsAt() {
+      return this.localAgendaItem.startsAt;
+    },
+  },
+
+  watch: {
+    startsAt(newValue, oldValue) {
+      let newStartTime = this.timeToMilliseconds(newValue);
+      let oldStartTime = this.timeToMilliseconds(oldValue);
+      let deltaStartTime = newStartTime - oldStartTime;
+      let oldEndTime = this.timeToMilliseconds(this.localAgendaItem.endsAt);
+      this.localAgendaItem.endsAt = new Date(oldEndTime + deltaStartTime).toISOString().substr(11, 5);
+    },
+
+    localAgendaItem: {
+      deep: true,
+      handler(newValue) {
+        if (!isEqual(newValue, this.agendaItem)) {
+          this.$emit('update:agendaItem', { ...newValue });
+        }
+      },
+    },
+  },
+
+  methods: {
+    timeToMilliseconds(time) {
+      time = time.split(':');
+      let hoursInMs = time[0] * 3600 * 1000;
+      let minutesInMs = time[1] * 60 * 1000;
+      return hoursInMs + minutesInMs;
     },
   },
 };
